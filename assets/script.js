c64b2b51e0c373a6547c2bf3add5dd1bff92b62a
Andrew Wilson <Wilsonandrew57@gmail.com>
2021-06-26 17:32:34 -0500

    all questions, timer/score working, needs formatting

--- assets/script.js
+++ assets/script.js
@@ -17,7 +17,7 @@ var btn4;
 // }
 
 function startQuiz() {
-    timerCount = 61;
+    timerCount = 60;
     startBtn.style.display = 'none';
     instructions.style.display = 'none'
     answerBtns();
@@ -31,7 +31,6 @@ function startTimer() {
         score.textContent = timerCount;
         if(timerCount < 0) {
             quizOver();
-            clearInterval(score);
         }
     }, 1000);
 }
@@ -49,10 +48,10 @@ function answerBtns() {
 }
 
 function question1() {
-    btn1.addEventListener("click", chkAns1);
-    btn2.addEventListener("click", chkAns1);
-    btn3.addEventListener("click", chkAns1);
-    btn4.addEventListener("click", chkAns1);
+    btn1.addEventListener("click", wrgAns1);
+    btn2.addEventListener("click", wrgAns1);
+    btn3.addEventListener("click", crtAns1);
+    btn4.addEventListener("click", wrgAns1);
 
     question.textContent = "Commonly used data types DO NOT include:"
     btn1.textContent = "Strings";
@@ -61,20 +60,28 @@ function question1() {
     btn4.textContent = "Booleans";
 }
 
-function chkAns1() {
-    if(btn3 === true) {
-        question2();
-    } else {
-        timerCount = timerCount - 10;
-        question2();
-    }
+function wrgAns1() {
+    timerCount = timerCount - 10;
+    btn1.removeEventListener("click", wrgAns1);
+    btn2.removeEventListener("click", wrgAns1);
+    btn3.removeEventListener("click", crtAns1);
+    btn4.removeEventListener("click", wrgAns1);
+    question2();
+}     
+
+function crtAns1() {
+    btn1.removeEventListener("click", wrgAns1);
+    btn2.removeEventListener("click", wrgAns1);
+    btn3.removeEventListener("click", crtAns1);
+    btn4.removeEventListener("click", wrgAns1);
+    question2();
 }
 
 function question2() {
-    btn1.addEventListener("click", chkAns2);
-    btn2.addEventListener("click", chkAns2);
-    btn3.addEventListener("click", chkAns2);
-    btn4.addEventListener("click", chkAns2);
+    btn1.addEventListener("click", wrgAns2);
+    btn2.addEventListener("click", crtAns2);
+    btn3.addEventListener("click", wrgAns2);
+    btn4.addEventListener("click", wrgAns2);
 
     question.textContent = "The condition in an if/else statement is inclosed with:"
     btn1.textContent = "Quotes";
@@ -83,16 +90,116 @@ function question2() {
     btn4.textContent = "Square Brackets";
 }
 
-function chkAns2() {
-    if(btn2 === true) {
-        quizOver();
-    } else {
-        timerCount = timerCount - 10;
-        quizOver();
-    }
+function wrgAns2() {
+    timerCount = timerCount - 10;
+    btn1.removeEventListener("click", wrgAns2);
+    btn2.removeEventListener("click", crtAns2);
+    btn3.removeEventListener("click", wrgAns2);
+    btn4.removeEventListener("click", wrgAns2);
+    question3();
+}
+
+function crtAns2() {
+    btn1.removeEventListener("click", wrgAns2);
+    btn2.removeEventListener("click", crtAns2);
+    btn3.removeEventListener("click", wrgAns2);
+    btn4.removeEventListener("click", wrgAns2);
+    question3();
+}
+
+function question3() {
+    btn1.addEventListener("click", wrgAns3);
+    btn2.addEventListener("click", wrgAns3);
+    btn3.addEventListener("click", wrgAns3);
+    btn4.addEventListener("click", crtAns3);
+
+    question.textContent = "Arrays in JavaScript can be used to store:"
+    btn1.textContent = "Numbers and Strings";
+    btn2.textContent = "Other arrays";
+    btn3.textContent = "Booleans";
+    btn4.textContent = "All of the above";
+}
+
+function wrgAns3() {
+    timerCount = timerCount - 10;
+    btn1.removeEventListener("click", wrgAns3);
+    btn2.removeEventListener("click", wrgAns3);
+    btn3.removeEventListener("click", wrgAns3);
+    btn4.removeEventListener("click", crtAns3);
+    question4();
+}
+
+function crtAns3() {
+    btn1.removeEventListener("click", wrgAns3);
+    btn2.removeEventListener("click", wrgAns3);
+    btn3.removeEventListener("click", wrgAns3);
+    btn4.removeEventListener("click", crtAns3);
+    question4();
+}
+
+function question4() {
+    btn1.addEventListener("click", wrgAns4);
+    btn2.addEventListener("click", wrgAns4);
+    btn3.addEventListener("click", crtAns4);
+    btn4.addEventListener("click", wrgAns4);
+
+    question.textContent = "String values need to be enclosed in:"
+    btn1.textContent = "Commas";
+    btn2.textContent = "Curly brackets";
+    btn3.textContent = "Quotes";
+    btn4.textContent = "Parentheses";
+}
+
+function wrgAns4() {
+    timerCount = timerCount - 10;
+    btn1.removeEventListener("click", wrgAns4);
+    btn2.removeEventListener("click", wrgAns4);
+    btn3.removeEventListener("click", crtAns4);
+    btn4.removeEventListener("click", wrgAns4);
+    question5();
+}
+
+function crtAns4() {
+    btn1.removeEventListener("click", wrgAns4);
+    btn2.removeEventListener("click", wrgAns4);
+    btn3.removeEventListener("click", crtAns4);
+    btn4.removeEventListener("click", wrgAns4);
+    question5();
+}
+
+function question5() {
+    btn1.addEventListener("click", wrgAns5);
+    btn2.addEventListener("click", wrgAns5);
+    btn3.addEventListener("click", wrgAns5);
+    btn4.addEventListener("click", crtAns5);
+
+    question.textContent = "What tool is used to print content to the debugger:"
+    btn1.textContent = "JavaScript";
+    btn2.textContent = "Terminal";
+    btn3.textContent = "For loops";
+    btn4.textContent = "Console.log";
+}
+
+function wrgAns5() {
+    timerCount = timerCount - 10;
+    btn1.removeEventListener("click", wrgAns5);
+    btn2.removeEventListener("click", wrgAns5);
+    btn3.removeEventListener("click", wrgAns5);
+    btn4.removeEventListener("click", crtAns5);
+    quizOver();
+}
+
+function crtAns5() {
+    btn1.removeEventListener("click", wrgAns5);
+    btn2.removeEventListener("click", wrgAns5);
+    btn3.removeEventListener("click", wrgAns5);
+    btn4.removeEventListener("click", crtAns5);
+    quizOver();
 }
 
 function quizOver() {
+    clearInterval(timer);
+    score.textContent = timerCount;
     question.textContent = "Your score is " + timerCount;
 }
 
